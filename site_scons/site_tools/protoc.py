0bcc71a0b11ea4f645083758b808f00c84ac53b0
Ali Mashtizadeh <mashti@cs.stanford.edu>
2011-12-12 16:15:47 +0000

    Updating protoc.py from bitbucket.org/russel/scons_protobuf/

--- site_scons/site_tools/protoc.py
+++ site_scons/site_tools/protoc.py
@@ -1,10 +1,36 @@
+#
+# Copyright (c) 2009  Scott Stafford
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+#  Author : Scott Stafford
+#  Date : 2009-12-09 20:36:14
+
 """ 
 protoc.py: Protoc Builder for SCons
 
-This Builder invokes protoc to generate C++ and Python 
-from a .proto file.  
+This Builder invokes protoc to generate C++ and Python  from a .proto file.  
 
-NOTE: Java is not currently supported."""
+NOTE: Java is not currently supported.
+"""
 
 __author__ = "Scott Stafford"
 
