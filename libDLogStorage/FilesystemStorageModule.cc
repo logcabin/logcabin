efcf70ebec01935f10abd9fcf05dbfa12f8ff8f6
Diego Ongaro <ongaro@cs.stanford.edu>
2012-01-02 19:40:22 -0800

    Clone DumbFilesystemStorageModule class as FilesystemStorageModule
    The Dumb one will remain a naive implementation; the other one is
    intended to grow into something useful.

---
+++ libDLogStorage/FilesystemStorageModule.cc
@@ -0,0 +1,230 @@
+/* Copyright (c) 2011 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#include <algorithm>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
+#include "../build/libDLogStorage/DumbFilesystem.pb.h"
+#include "../dlogd/Debug.h" // TODO(ongaro): Move Debug to common
+#include "FilesystemStorageModule.h"
+#include "FilesystemUtil.h"
+
+namespace DLog {
+
+namespace Storage {
+
+// class FilesystemStorageModule
+
+FilesystemStorageModule::FilesystemStorageModule(
+                                    const std::string& path)
+    : path(path)
+{
+    if (mkdir(path.c_str(), 0755) != 0) {
+        if (errno != EEXIST) {
+            PANIC("Failed to create directory for FilesystemStorageModule:"
+                  " mkdir(%s) failed: %s", path.c_str(), strerror(errno));
+        }
+    }
+}
+
+std::vector<LogId>
+FilesystemStorageModule::getLogs()
+{
+    std::vector<std::string> filenames = FilesystemUtil::ls(path);
+    std::vector<LogId> ret;
+    for (auto it = filenames.begin(); it != filenames.end(); ++it) {
+        const std::string& filename = *it;
+        LogId logId;
+        unsigned bytesConsumed;
+        int matched = sscanf(filename.c_str(), "%016lx%n", // NOLINT
+                             &logId, &bytesConsumed);
+        if (matched != 1 || bytesConsumed != filename.length()) {
+            WARN("%s doesn't look like a valid log ID (from %s)",
+                 filename.c_str(),
+                 (path + "/" + filename).c_str());
+            continue;
+        }
+        ret.push_back(logId);
+    }
+    return ret;
+}
+
+Ref<Log>
+FilesystemStorageModule::openLog(LogId logId)
+{
+    Ref<Log> newLog = make<FilesystemLog>(logId, getLogPath(logId));
+    return newLog;
+}
+
+void
+FilesystemStorageModule::deleteLog(LogId logId,
+                               Ref<DeleteCallback> deleteCompletion)
+{
+    FilesystemUtil::remove(getLogPath(logId));
+    deleteCompletion->deleted(logId);
+}
+
+std::string
+FilesystemStorageModule::getLogPath(LogId logId) const
+{
+    return format("%s/%016lx", path.c_str(), logId);
+}
+
+// class FilesystemLog
+
+FilesystemLog::FilesystemLog(LogId logId, const std::string& path)
+    : Log(logId)
+    , path(path)
+    , headId(NO_ENTRY_ID)
+    , entries()
+{
+    if (mkdir(path.c_str(), 0755) != 0) {
+        if (errno != EEXIST) {
+            PANIC("Failed to create directory for FilesystemLog:"
+                  " mkdir(%s) failed: %s", path.c_str(), strerror(errno));
+        }
+    }
+
+    std::vector<EntryId> entryIds = getEntryIds();
+    std::sort(entryIds.begin(), entryIds.end());
+    for (auto it = entryIds.begin(); it != entryIds.end(); ++it)
+        read(*it);
+}
+
+std::deque<LogEntry>
+FilesystemLog::readFrom(EntryId start)
+{
+    std::deque<LogEntry> ret;
+    for (auto it = entries.rbegin();
+         it != entries.rend();
+         ++it) {
+        LogEntry& entry = *it;
+        if (entry.entryId < start)
+            break;
+        ret.push_front(entry);
+    }
+    return ret;
+}
+
+void
+FilesystemLog::append(LogEntry& entry,
+                          Ref<AppendCallback> appendCompletion)
+{
+    EntryId newId = headId + 1;
+    if (headId == NO_ENTRY_ID)
+        newId = 0;
+    headId = newId;
+    entry.logId = getLogId();
+    entry.entryId = newId;
+    entries.push_back(entry);
+    write(entry);
+    appendCompletion->appended(entries.back());
+}
+
+std::vector<EntryId>
+FilesystemLog::getEntryIds()
+{
+    std::vector<std::string> filenames = FilesystemUtil::ls(path);
+    std::vector<EntryId> entryIds;
+    for (auto it = filenames.begin(); it != filenames.end(); ++it) {
+        const std::string& filename = *it;
+        EntryId entryId;
+        unsigned bytesConsumed;
+        int matched = sscanf(filename.c_str(), "%016lx%n", // NOLINT
+                             &entryId, &bytesConsumed);
+        if (matched != 1 || bytesConsumed != filename.length()) {
+            WARN("%s doesn't look like a valid entry ID (from %s)",
+                 filename.c_str(),
+                 (path + "/" + filename).c_str());
+            continue;
+        }
+        entryIds.push_back(entryId);
+    }
+    return entryIds;
+}
+
+std::string
+FilesystemLog::getEntryPath(EntryId entryId) const
+{
+    return format("%s/%016lx", path.c_str(), entryId);
+}
+
+void
+FilesystemLog::read(EntryId entryId)
+{
+    const std::string entryPath = getEntryPath(entryId);
+    int fd = open(entryPath.c_str(), O_RDONLY);
+    if (fd == -1)
+        PANIC("Could not open %s: %s", entryPath.c_str(), strerror(errno));
+    ::google::protobuf::io::FileInputStream inputStream(fd);
+    inputStream.SetCloseOnDelete(true);
+    ProtoBuf::DumbFilesystem::LogEntry contents;
+    if (!contents.ParseFromZeroCopyStream(&inputStream)) {
+        PANIC("Failed to parse log entry from %s. "
+              "It is missing the following required fields: %s",
+              entryPath.c_str(),
+              contents.InitializationErrorString().c_str());
+    }
+    Ref<Chunk> data = NO_DATA;
+    if (contents.has_data()) {
+        const std::string& contentsData = contents.data();
+        data = Chunk::makeChunk(contentsData.c_str(),
+                                downCast<uint32_t>(contentsData.length()));
+    }
+    const auto& contentsInv = contents.invalidations();
+    std::vector<EntryId> invalidations(contentsInv.begin(), contentsInv.end());
+    LogEntry entry(getLogId(),
+                   entryId,
+                   contents.create_time(),
+                   data,
+                   invalidations);
+    entries.push_back(entry);
+    if (headId == NO_ENTRY_ID || headId < entryId)
+        headId = entryId;
+}
+
+void
+FilesystemLog::write(const LogEntry& entry)
+{
+    ProtoBuf::DumbFilesystem::LogEntry contents;
+    contents.set_create_time(entry.createTime);
+    if (entry.data != NO_DATA) {
+        contents.set_data(entry.data->getData(),
+                          entry.data->getLength());
+    }
+    for (auto it = entry.invalidations.begin();
+         it != entry.invalidations.end();
+         ++it) {
+        contents.add_invalidations(*it);
+    }
+
+    const std::string entryPath = getEntryPath(entry.entryId);
+    int fd = open(entryPath.c_str(), O_WRONLY|O_CREAT|O_EXCL, 0644);
+    if (fd == -1)
+        PANIC("Could not create %s: %s", entryPath.c_str(), strerror(errno));
+    ::google::protobuf::io::FileOutputStream outputStream(fd);
+    outputStream.SetCloseOnDelete(true);
+    if (!contents.SerializeToZeroCopyStream(&outputStream))
+        PANIC("Failed to serialize log entry");
+}
+
+
+} // namespace DLog::Storage
+} // namespace DLog
---
+++ test/libDLogStorage/FilesystemStorageModule.cc
@@ -0,0 +1,299 @@
+/* Copyright (c) 2011 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+#include "Common.h"
+#include "dlogd/Debug.h"
+#include "libDLogStorage/FilesystemStorageModule.h"
+#include "libDLogStorage/FilesystemUtil.h"
+
+namespace DLog {
+namespace Storage {
+
+using std::string;
+using std::vector;
+using std::deque;
+
+namespace {
+
+class LogAppendCallback : public Log::AppendCallback {
+  private:
+    LogAppendCallback() = default;
+  public:
+    void appended(LogEntry entry) {
+        lastEntry = entry;
+    }
+    static LogEntry lastEntry;
+};
+LogEntry LogAppendCallback::lastEntry {
+    0xdeadbeef,
+    0xdeadbeef,
+    0xdeadbeefdeadbeef,
+    Chunk::makeChunk("deadbeef", 9),
+    { 0xdeadbeef }
+};
+
+class SMDeleteCallback : public StorageModule::DeleteCallback {
+  private:
+    SMDeleteCallback() = default;
+  public:
+    void deleted(LogId logId) {
+        lastLogId = logId;
+    }
+    static LogId lastLogId;
+    friend class MakeHelper;
+    friend class RefHelper<SMDeleteCallback>;
+};
+LogId SMDeleteCallback::lastLogId;
+
+template<typename Container>
+vector<string>
+eStr(const Container& container)
+{
+    vector<string> ret;
+    for (auto it = container.begin();
+         it != container.end();
+         ++it) {
+        ret.push_back(it->toString());
+    }
+    return ret;
+}
+
+} // anonymous namespace
+
+class FilesystemStorageModuleTest : public ::testing::Test {
+  public:
+    FilesystemStorageModuleTest()
+        : tmpdir(FilesystemUtil::tmpnam())
+        , sm() {
+        SMDeleteCallback::lastLogId = 0;
+    }
+    ~FilesystemStorageModuleTest() {
+        FilesystemUtil::remove(tmpdir);
+    }
+    void createStorageModule() {
+        sm = make<FilesystemStorageModule>(tmpdir);
+    }
+    std::string tmpdir;
+    Ptr<FilesystemStorageModule> sm;
+};
+
+TEST_F(FilesystemStorageModuleTest, constructor) {
+    createStorageModule();
+    createStorageModule(); // no error if the directory already exists
+    EXPECT_DEATH(make<FilesystemStorageModule>(
+                            "/the/parent/directory/doesnt/exist"),
+                 "Failed to create directory");
+}
+
+TEST_F(FilesystemStorageModuleTest, getLogs) {
+    createStorageModule();
+    EXPECT_EQ((vector<LogId>{}), sorted(sm->getLogs()));
+    sm->openLog(38);
+    sm->openLog(755);
+    sm->openLog(129);
+    EXPECT_EQ((vector<LogId>{38, 129, 755}), sorted(sm->getLogs()));
+    close(open((tmpdir + "/NaN").c_str(), O_WRONLY|O_CREAT, 0644));
+    createStorageModule();
+    EXPECT_EQ((vector<LogId>{38, 129, 755}), sorted(sm->getLogs()));
+}
+
+TEST_F(FilesystemStorageModuleTest, openLog) {
+    createStorageModule();
+    Ref<Log> log = sm->openLog(12);
+    EXPECT_EQ(12U, log->getLogId());
+    EXPECT_EQ((vector<LogId>{12}), sorted(sm->getLogs()));
+    createStorageModule();
+    EXPECT_EQ((vector<LogId>{12}), sorted(sm->getLogs()));
+}
+
+TEST_F(FilesystemStorageModuleTest, deleteLog) {
+    createStorageModule();
+    Ref<Log> log = sm->openLog(12);
+    sm->deleteLog(10, make<SMDeleteCallback>());
+    EXPECT_EQ(10U, SMDeleteCallback::lastLogId);
+    sm->deleteLog(12, make<SMDeleteCallback>());
+    EXPECT_EQ(12U, SMDeleteCallback::lastLogId);
+    EXPECT_EQ((vector<LogId>{}), sorted(sm->getLogs()));
+    createStorageModule();
+    EXPECT_EQ((vector<LogId>{}), sorted(sm->getLogs()));
+}
+
+TEST_F(FilesystemStorageModuleTest, getLogPath) {
+    createStorageModule();
+    EXPECT_EQ(tmpdir + "/000000000000001f", sm->getLogPath(31));
+}
+
+class FilesystemLogTest : public FilesystemStorageModuleTest {
+  public:
+    FilesystemLogTest()
+        : log()
+    {
+        createStorageModule();
+        createLog();
+    }
+    void createLog() {
+        Ref<Log> tmpLog = sm->openLog(92);
+        log = Ptr<FilesystemLog>(
+                        static_cast<FilesystemLog*>(tmpLog.get()));
+    }
+    Ptr<FilesystemLog> log;
+};
+
+TEST_F(FilesystemLogTest, constructor) {
+    EXPECT_EQ(92U, log->getLogId());
+    EXPECT_EQ(tmpdir + "/000000000000005c", log->path);
+    EXPECT_EQ(NO_ENTRY_ID, log->headId);
+
+    createLog(); // no error if the directory already exists
+    EXPECT_DEATH(make<FilesystemLog>(444,
+                            "/the/parent/directory/doesnt/exist"),
+                 "Failed to create directory");
+
+    LogEntry e1(1, 2, 3, Chunk::makeChunk("hello", 6));
+    log->append(e1, make<LogAppendCallback>());
+    LogEntry e2(4, 5, 6, Chunk::makeChunk("goodbye", 8));
+    log->append(e2, make<LogAppendCallback>());
+    createLog();
+    EXPECT_EQ((vector<string> {
+                "(92, 0) 'hello'",
+                "(92, 1) 'goodbye'",
+              }),
+              eStr(log->entries));
+}
+
+TEST_F(FilesystemLogTest, getLastId) {
+    EXPECT_EQ(NO_ENTRY_ID, log->getLastId());
+    LogEntry e1(1, 2, 3, Chunk::makeChunk("hello", 6));
+    log->append(e1, make<LogAppendCallback>());
+    EXPECT_EQ(0U, log->getLastId());
+    log->append(e1, make<LogAppendCallback>());
+    EXPECT_EQ(1U, log->getLastId());
+    createLog();
+    EXPECT_EQ(1U, log->getLastId());
+}
+
+TEST_F(FilesystemLogTest, readFrom) {
+    EXPECT_EQ(vector<string>{}, eStr(log->readFrom(0)));
+    EXPECT_EQ(vector<string>{}, eStr(log->readFrom(12)));
+    LogEntry e1(1, 2, 3, Chunk::makeChunk("hello", 6));
+    log->append(e1, make<LogAppendCallback>());
+    LogEntry e2(4, 5, 6, Chunk::makeChunk("world!", 7));
+    log->append(e2, make<LogAppendCallback>());
+    EXPECT_EQ((vector<string> {
+                "(92, 0) 'hello'",
+                "(92, 1) 'world!'",
+              }),
+              eStr(log->readFrom(0)));
+    EXPECT_EQ((vector<string> {
+                "(92, 1) 'world!'",
+              }),
+              eStr(log->readFrom(1)));
+    EXPECT_EQ((vector<string> {}),
+              eStr(log->readFrom(2)));
+    createLog();
+    EXPECT_EQ((vector<string> {
+                "(92, 0) 'hello'",
+                "(92, 1) 'world!'",
+              }),
+              eStr(log->readFrom(0)));
+}
+
+TEST_F(FilesystemLogTest, append) {
+    LogEntry e1(1, 2, 3, Chunk::makeChunk("hello", 6), {4, 5});
+    log->append(e1, make<LogAppendCallback>());
+    EXPECT_EQ(92U, e1.logId);
+    EXPECT_EQ(0U, e1.entryId);
+    EXPECT_EQ("(92, 0) 'hello' [inv 4, 5]",
+              LogAppendCallback::lastEntry.toString());
+    LogEntry e2(1, 2, 3, Chunk::makeChunk("goodbye", 8), {4, 5});
+    log->append(e2, make<LogAppendCallback>());
+    EXPECT_EQ(1U, e2.entryId);
+    createLog();
+    EXPECT_EQ(1U, log->getLastId());
+}
+
+TEST_F(FilesystemLogTest, getEntryIds) {
+    EXPECT_EQ((vector<EntryId>{}), sorted(log->getEntryIds()));
+    LogEntry e1(1, 2, 3, Chunk::makeChunk("hello", 6));
+    log->append(e1, make<LogAppendCallback>());
+    LogEntry e2(4, 5, 6, Chunk::makeChunk("goodbye", 8));
+    log->append(e2, make<LogAppendCallback>());
+    EXPECT_EQ((vector<LogId>{0, 1}), sorted(log->getEntryIds()));
+    close(open((log->path + "/NaN").c_str(), O_WRONLY|O_CREAT, 0644));
+    createLog();
+    EXPECT_EQ((vector<LogId>{0, 1}), sorted(log->getEntryIds()));
+}
+
+TEST_F(FilesystemLogTest, getEntryPath) {
+    EXPECT_EQ(log->path + "/000000000000001f", log->getEntryPath(31));
+}
+
+TEST_F(FilesystemLogTest, readErrors) {
+    EXPECT_DEATH(log->read(444),
+                 "Could not open");
+
+    close(open((log->path + "/0000000000000000").c_str(),
+               O_WRONLY|O_CREAT, 0644));
+    EXPECT_DEATH(log->read(0),
+                 "Failed to parse log entry");
+}
+
+TEST_F(FilesystemLogTest, writeErrors) {
+    LogEntry e1(92, 1, 5, NO_DATA);
+    log->write(e1);
+    EXPECT_DEATH(log->write(e1),
+                 "Could not create");
+
+    // TODO(ongaro): Test a failure in serializing the protocol buffer.
+    // I don't see an obvious, clean way to do this.
+}
+
+TEST_F(FilesystemLogTest, readWriteCommon) {
+    LogEntry e1(92, 1, 5, NO_DATA);
+    log->write(e1);
+    log->read(1);
+    LogEntry e2(92, 2, 6, Chunk::makeChunk("hello", 6));
+    log->write(e2);
+    log->read(2);
+    LogEntry e3(92, 3, 7, NO_DATA, vector<EntryId>{28, 29, 30});
+    log->write(e3);
+    log->read(3);
+    LogEntry e4(92, 4, 8, Chunk::makeChunk("hello", 6),
+                vector<EntryId>{31, 33, 94});
+    log->write(e4);
+    log->read(4);
+    // empty data should differ from NO_DATA
+    LogEntry e5(92, 5, 9, Chunk::makeChunk("", 0));
+    log->write(e5);
+    log->read(5);
+    EXPECT_EQ((vector<string> {
+                "(92, 1) NODATA",
+                "(92, 2) 'hello'",
+                "(92, 3) NODATA [inv 28, 29, 30]",
+                "(92, 4) 'hello' [inv 31, 33, 94]",
+                "(92, 5) ''",
+              }),
+              eStr(log->entries));
+    EXPECT_EQ(9U, log->entries.back().createTime);
+    EXPECT_EQ(5U, log->headId);
+}
+
+} // namespace DLog::Storage
+} // namespace DLog
