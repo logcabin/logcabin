logcabin/logcabin

commit fc4d35e34b6fd6eabebd007a422fe31924be831b

Fix 100% CPU and wedges with gcc 4.8
This was caused by http://gcc.gnu.org/bugzilla/show_bug.cgi?id=58931
In short, waiting for a monotonic clock's infinity would overflow,
causing no wait at all. Worse yet, the mutex wouldn't even be released
and reacquired, so all other threads get starved.

The work-around in this commit is to sleep for no longer than one hour
(after an hour, you'll get a spurious interrupt).

Fix #49: 100% cpu in event loop
Fix #50: leader process hangs after Conrol-C

Core/ConditionVariable.h
@@ -21,9 +21,17 @@
 #include <condition_variable>
 #include <functional>
 
+#ifndef LOGCABIN_CORE_CONDITIONVARIABLE_H
+#define LOGCABIN_CORE_CONDITIONVARIABLE_H
+
+namespace LogCabin {
+namespace Core {
+
 /**
  * A wrapper around std::condition_variable that is useful for testing
- * purposes. You can set a callback to be called when the condition variable is
+ * purposes and works around a bug in libstdc++.
+ *
+ * You can set a callback to be called when the condition variable is
  * waited on; instead of waiting, this callback will be called. This callback
  * can, for example, change some shared state so that the calling thread's
  * condition is satisfied. It also counts how many times the condition variable
@@ -34,12 +42,6 @@
  * std::unique_lock<Mutex>, since normal condition variables may only be used
  * with std::unique_lock<std::mutex>.
  */
-#ifndef LOGCABIN_CORE_CONDITIONVARIABLE_H
-#define LOGCABIN_CORE_CONDITIONVARIABLE_H
-
-namespace LogCabin {
-namespace Core {
-
 class ConditionVariable {
   public:
     ConditionVariable()
@@ -107,7 +109,13 @@ class ConditionVariable {
             callback();
             lockGuard.lock();
         } else {
-            cv.wait_until(lockGuard, abs_time);
+            // Work around overflow in libstdc++, see
+            // http://gcc.gnu.org/bugzilla/show_bug.cgi?id=58931
+            if (abs_time - Clock::now() > std::chrono::hours(1)) {
+                cv.wait_until(lockGuard, Clock::now() + std::chrono::hours(1));
+            } else {
+                cv.wait_until(lockGuard, abs_time);
+            }
         }
     }
 
@@ -133,7 +141,8 @@ class ConditionVariable {
     std::condition_variable cv;
     /**
      * This function will be called with the lock released during every
-     * invocation of wait/wait_until.
+     * invocation of wait/wait_until. No wait will actually occur; this is only
+     * used for unit testing.
      */
     std::function<void()> callback;
   public:
@@ -143,7 +152,8 @@ class ConditionVariable {
     std::atomic<uint64_t> notificationCount;
     /**
      * In the last call to wait_until, the timeout that the caller provided.
-     * This is stored as the number of milliseconds after the
+     * This is used in some unit tests to check that timeouts are set
+     * correctly. It is stored as the number of milliseconds after the
      * clock's epoch. (Since we don't know the Clock in advance, we can't store
      * a time_point here.)
      */

commit 1b4564a8bc0ffc7afe12c61301becea57949fa79

[ConditionVariable] Fix wait_until again
This regression was introduced in fc4d35e: "Fix 100% CPU and wedges with
gcc 4.8". Waiting until TimePoint::min() should return immediately but
did not after that commit.

This time around I added unit tests for the entire ConditionVariable
class, so I think it's right now :)

Core/ConditionVariable.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012 Stanford University
+/* Copyright (c) 2012-2014 Stanford University
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -90,6 +90,8 @@ class ConditionVariable {
         assert(stdLockGuard);
         lockGuard = std::unique_lock<Core::Mutex>(mutex, std::adopt_lock_t());
         stdLockGuard.release();
+        if (mutex.callback)
+            mutex.callback();
     }
 
 
@@ -111,13 +113,14 @@ class ConditionVariable {
             callback();
             lockGuard.lock();
         } else {
-            // Work around overflow in libstdc++, see
+            // Work-around for bug in libstdc++: wait for no more than an hour
+            // to avoid overflow. See
             // http://gcc.gnu.org/bugzilla/show_bug.cgi?id=58931
-            if (abs_time - Clock::now() > std::chrono::hours(1)) {
-                cv.wait_until(lockGuard, Clock::now() + std::chrono::hours(1));
-            } else {
+            std::chrono::time_point<Clock, Duration> now = Clock::now();
+            if (abs_time < now + std::chrono::hours(1))
                 cv.wait_until(lockGuard, abs_time);
-            }
+            else
+                cv.wait_until(lockGuard, now + std::chrono::hours(1));
         }
     }
 
@@ -136,6 +139,8 @@ class ConditionVariable {
         assert(stdLockGuard);
         lockGuard = std::unique_lock<Core::Mutex>(mutex, std::adopt_lock_t());
         stdLockGuard.release();
+        if (mutex.callback)
+            mutex.callback();
     }
 
   private:

Core/ConditionVariableTest.cc
@@ -1,0 +1,346 @@
+/* Copyright (c) 2014 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <gtest/gtest.h>
+#include <thread>
+
+#include "Core/ConditionVariable.h"
+#include "Core/Time.h"
+
+namespace LogCabin {
+namespace Core {
+namespace {
+
+typedef std::chrono::milliseconds ms;
+
+// Many of these tests are very sensitive to timing and system load.
+// That's just the nature of testing condition variables.
+
+class CoreConditionVariableTest : public ::testing::Test {
+    CoreConditionVariableTest()
+        : mutex()
+        , stdmutex()
+        , cv()
+        , ready(0)
+        , done(0)
+        , counter1(0)
+        , counter2(0)
+        , mutexCounter(0)
+        , cond(false)
+        , thread1()
+        , thread2()
+    {
+        mutex.callback =
+            std::bind(&CoreConditionVariableTest::incrementMutexCounter, this);
+    }
+    ~CoreConditionVariableTest()
+    {
+        cv.notify_all();
+        if (thread1.joinable())
+            thread1.join();
+        if (thread2.joinable())
+            thread2.join();
+    }
+
+    void wait()
+    {
+        std::unique_lock<Mutex> lockGuard(mutex);
+        ++ready;
+        while (!cond) {
+            cv.wait(lockGuard);
+            ++counter2;
+        }
+        ++done;
+    }
+
+    void waitStdMutex()
+    {
+        std::unique_lock<std::mutex> lockGuard(stdmutex);
+        ++ready;
+        while (!cond) {
+            cv.wait(lockGuard);
+            ++counter2;
+        }
+        ++done;
+    }
+
+    void incrementCounter1()
+    {
+        ++counter1;
+    }
+
+    void incrementCounter2()
+    {
+        ++counter2;
+    }
+
+    void incrementMutexCounter()
+    {
+        ++mutexCounter;
+    }
+
+    void spinForReady(uint64_t until)
+    {
+        for (uint64_t i = 0; i < 100; ++i) {
+            if (ready >= until)
+                return;
+            usleep(1000);
+        }
+        PANIC("timeout");
+    }
+
+    void sleepAndNotify(uint64_t ms, std::atomic<bool>& exit)
+    {
+        for (uint64_t i = 0; i < ms; ++i) {
+            if (exit)
+                return;
+            usleep(1000);
+        }
+        cv.notify_all();
+    }
+
+    template<typename LockGuard, typename Clock, typename Duration>
+    Duration
+    timedWaitUntil(LockGuard& lockGuard,
+                   const std::chrono::time_point<Clock, Duration>& abs_time,
+                   uint64_t watchdogMs = 50)
+    {
+        std::atomic<bool> watchdogExit(false);
+        std::thread watchdog(&CoreConditionVariableTest::sleepAndNotify,
+                             this, watchdogMs, std::ref(watchdogExit));
+        typename Clock::time_point start = Clock::now();
+        cv.wait_until(lockGuard, abs_time);
+        typename Clock::time_point end = Clock::now();
+        watchdogExit = true;
+        watchdog.join();
+        return end - start;
+    }
+
+    Mutex mutex;
+    std::mutex stdmutex;
+    ConditionVariable cv;
+    std::atomic<uint64_t> ready;
+    std::atomic<uint64_t> done;
+    std::atomic<uint64_t> counter1;
+    std::atomic<uint64_t> counter2;
+    std::atomic<uint64_t> mutexCounter;
+    std::atomic<bool> cond;
+    std::thread thread1;
+    std::thread thread2;
+};
+
+
+TEST_F(CoreConditionVariableTest, notify_one) {
+    cv.notify_one();
+    EXPECT_EQ(1U, cv.notificationCount);
+    thread1 = std::thread(&CoreConditionVariableTest::wait, this);
+    thread2 = std::thread(&CoreConditionVariableTest::wait, this);
+    spinForReady(2);
+    EXPECT_EQ(0U, done);
+    {
+        std::unique_lock<Mutex> lockGuard(mutex);
+        cond = true;
+        cv.notify_one();
+    }
+    EXPECT_EQ(2U, cv.notificationCount);
+    usleep(1000);
+    EXPECT_EQ(1U, counter2);
+    EXPECT_EQ(1U, done);
+    {
+        std::unique_lock<Mutex> lockGuard(mutex);
+        cond = true;
+        cv.notify_one();
+    }
+    usleep(1000);
+    EXPECT_EQ(2U, counter2);
+    EXPECT_EQ(2U, done);
+    thread1.join();
+    thread2.join();
+}
+
+TEST_F(CoreConditionVariableTest, notify_all) {
+    cv.notify_all();
+    EXPECT_EQ(1U, cv.notificationCount);
+    thread1 = std::thread(&CoreConditionVariableTest::wait, this);
+    thread2 = std::thread(&CoreConditionVariableTest::wait, this);
+    spinForReady(2);
+    EXPECT_EQ(0U, done);
+    {
+        std::unique_lock<Mutex> lockGuard(mutex);
+        cond = true;
+        cv.notify_all();
+    }
+    EXPECT_EQ(2U, cv.notificationCount);
+    thread1.join();
+    thread2.join();
+    EXPECT_EQ(2U, counter2);
+    EXPECT_EQ(2U, done);
+}
+
+TEST_F(CoreConditionVariableTest, wait_stdmutex_callback) {
+    cv.callback = std::bind(&CoreConditionVariableTest::incrementCounter1,
+                            this);
+    std::unique_lock<std::mutex> lockGuard(stdmutex);
+    cv.wait(lockGuard);
+    EXPECT_EQ(1U, counter1);
+}
+
+TEST_F(CoreConditionVariableTest, wait_stdmutex_real) {
+    thread1 = std::thread(&CoreConditionVariableTest::waitStdMutex, this);
+    spinForReady(1);
+    EXPECT_EQ(0U, done);
+    {
+        std::unique_lock<std::mutex> lockGuard(stdmutex);
+        cond = true;
+        cv.notify_all();
+    }
+    thread1.join();
+    EXPECT_EQ(1U, counter2);
+}
+
+TEST_F(CoreConditionVariableTest, wait_CoreMutex_callback) {
+    cv.callback = std::bind(&CoreConditionVariableTest::incrementCounter1,
+                            this);
+    {
+        std::unique_lock<Mutex> lockGuard(mutex);
+        cv.wait(lockGuard);
+    }
+    EXPECT_EQ(1U, counter1);
+    EXPECT_EQ(4U, mutexCounter); // 2 from lock guard, 2 from wait
+}
+
+TEST_F(CoreConditionVariableTest, wait_CoreMutex_real) {
+    thread1 = std::thread(&CoreConditionVariableTest::wait, this);
+    spinForReady(1);
+    EXPECT_EQ(0U, done);
+    {
+        std::unique_lock<Mutex> lockGuard(mutex);
+        cond = true;
+        cv.notify_all();
+    }
+    thread1.join();
+    EXPECT_EQ(1U, counter2);
+    EXPECT_EQ(6U, mutexCounter); // 4 from lock guards, 2 from wait
+}
+
+
+TEST_F(CoreConditionVariableTest, wait_until_stdmutex_callback) {
+    cv.callback = std::bind(&CoreConditionVariableTest::incrementCounter1,
+                            this);
+    std::unique_lock<std::mutex> lockGuard(stdmutex);
+
+    cv.wait_until(lockGuard, Time::SteadyClock::time_point::min());
+    cv.wait_until(lockGuard, Time::SteadyClock::now() - ms(1));
+    cv.wait_until(lockGuard, Time::SteadyClock::now());
+    cv.wait_until(lockGuard, Time::SteadyClock::now() + ms(1));
+    cv.wait_until(lockGuard, Time::SteadyClock::time_point::max());
+    EXPECT_EQ(5U, counter1);
+    counter1 = 0;
+
+    cv.wait_until(lockGuard, Time::SystemClock::time_point::min());
+    cv.wait_until(lockGuard, Time::SystemClock::now() - ms(1));
+    cv.wait_until(lockGuard, Time::SystemClock::now());
+    cv.wait_until(lockGuard, Time::SystemClock::now() + ms(1));
+    cv.wait_until(lockGuard, Time::SystemClock::time_point::max());
+    EXPECT_EQ(5U, counter1);
+}
+
+TEST_F(CoreConditionVariableTest, wait_until_stdmutex_real) {
+    std::unique_lock<std::mutex> lockGuard(stdmutex);
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SteadyClock::time_point::min()),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SteadyClock::now() - ms(1)),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SteadyClock::now()),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SteadyClock::now() + ms(1)),
+              ms(2));
+    EXPECT_GT(timedWaitUntil(lockGuard, Time::SteadyClock::now() + ms(10), 2),
+              ms(1));
+    EXPECT_GT(timedWaitUntil(lockGuard,
+                             Time::SteadyClock::time_point::max(), 2), ms(1));
+
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SystemClock::time_point::min()),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SystemClock::now() - ms(1)),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SystemClock::now()),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SystemClock::now() + ms(1)),
+              ms(2));
+    EXPECT_GT(timedWaitUntil(lockGuard, Time::SystemClock::now() + ms(10), 2),
+              ms(1));
+    EXPECT_GT(timedWaitUntil(lockGuard,
+                             Time::SystemClock::time_point::max(), 2), ms(1));
+}
+
+TEST_F(CoreConditionVariableTest, wait_until_CoreMutex_callback) {
+    cv.callback = std::bind(&CoreConditionVariableTest::incrementCounter1,
+                            this);
+    {
+        std::unique_lock<Core::Mutex> lockGuard(mutex);
+
+        cv.wait_until(lockGuard, Time::SteadyClock::time_point::min());
+        cv.wait_until(lockGuard, Time::SteadyClock::now() - ms(1));
+        cv.wait_until(lockGuard, Time::SteadyClock::now());
+        cv.wait_until(lockGuard, Time::SteadyClock::now() + ms(1));
+        cv.wait_until(lockGuard, Time::SteadyClock::time_point::max());
+        EXPECT_EQ(5U, counter1);
+        counter1 = 0;
+
+        cv.wait_until(lockGuard, Time::SystemClock::time_point::min());
+        cv.wait_until(lockGuard, Time::SystemClock::now() - ms(1));
+        cv.wait_until(lockGuard, Time::SystemClock::now());
+        cv.wait_until(lockGuard, Time::SystemClock::now() + ms(1));
+        cv.wait_until(lockGuard, Time::SystemClock::time_point::max());
+        EXPECT_EQ(5U, counter1);
+    }
+    EXPECT_EQ(22U, mutexCounter); // 2 from lock guard, 20 from wait_until
+}
+
+TEST_F(CoreConditionVariableTest, wait_until_CoreMutex_real) {
+    std::unique_lock<Core::Mutex> lockGuard(mutex);
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SteadyClock::time_point::min()),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SteadyClock::now() - ms(1)),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SteadyClock::now()),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SteadyClock::now() + ms(1)),
+              ms(2));
+    EXPECT_GT(timedWaitUntil(lockGuard, Time::SteadyClock::now() + ms(10), 2),
+              ms(1));
+    EXPECT_GT(timedWaitUntil(lockGuard,
+                             Time::SteadyClock::time_point::max(), 2), ms(1));
+
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SystemClock::time_point::min()),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SystemClock::now() - ms(1)),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SystemClock::now()),
+              ms(1));
+    EXPECT_LT(timedWaitUntil(lockGuard, Time::SystemClock::now() + ms(1)),
+              ms(2));
+    EXPECT_GT(timedWaitUntil(lockGuard, Time::SystemClock::now() + ms(10), 2),
+              ms(1));
+    EXPECT_GT(timedWaitUntil(lockGuard,
+                             Time::SystemClock::time_point::max(), 2), ms(1));
+    lockGuard.unlock();
+    EXPECT_EQ(26U, mutexCounter); // 2 from lock guard, 24 from wait_until
+}
+
+} // namespace LogCabin::Core::<anonymous>
+} // namespace LogCabin::Core
+} // namespace LogCabin