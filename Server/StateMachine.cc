logcabin/logcabin

commit 10b42c6ffed9755a525c25856f0aa4d61d563bc4

StateMachine: Expire client sessions after about an hour of inactivity
The leader adds its local time to each command and puts this in the log;
state machines on different servers use this to expire clients
deterministically (that way the state machines don't diverge).

Closes #31: StateMachine needs unit tests.

Server/StateMachine.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013 Stanford University
+/* Copyright (c) 2012-2014 Stanford University
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -44,6 +44,13 @@ StateMachine::StateMachine(std::shared_ptr<Consensus> consensus,
     : consensus(consensus)
     , snapshotMinLogSize(config.read<uint64_t>("snapshotMinLogSize", 1024))
     , snapshotRatio(config.read<uint64_t>("snapshotRatio", 10))
+      // TODO(ongaro): This should be configurable, but it must be the same for
+      // every server, so it's dangerous to put it in the config file. Need to
+      // use the Raft log to agree on this value. Also need to inform clients
+      // of the value and its changes, so that they can send keep-alives at
+      // appropriate intervals. For now, servers time out after an hour, and
+      // clients send keep-alives every minute.
+    , sessionTimeoutNanos(1000UL * 1000 * 1000 * 60 * 60)
     , mutex()
     , entriesApplied()
     , snapshotSuggested()
@@ -120,38 +127,50 @@ StateMachine::wait(uint64_t entryId) const
 void
 StateMachine::apply(uint64_t entryId, const std::string& data)
 {
+    // TODO(ongaro): Switch from string to binary format. This is probably
+    // really slow to parse.
     PC::Command command = Core::ProtoBuf::fromString<PC::Command>(data);
-    PC::CommandResponse commandResponse;
-    PC::ExactlyOnceRPCInfo rpcInfo;
+    Session* session = NULL;
     if (command.has_tree()) {
-        rpcInfo = command.tree().exactly_once();
-        if (ignore(rpcInfo))
-            return;
-        Tree::ProtoBuf::readWriteTreeRPC(
-            tree, command.tree(), *commandResponse.mutable_tree());
+        PC::ExactlyOnceRPCInfo rpcInfo = command.tree().exactly_once();
+        auto it = sessions.find(rpcInfo.client_id());
+        if (it == sessions.end()) {
+            // session does not exist
+        } else {
+            // session exists
+            session = &it->second;
+            expireResponses(*session, rpcInfo.first_outstanding_rpc());
+            if (rpcInfo.rpc_number() < session->firstOutstandingRPC) {
+                // response already discarded, do not re-apply
+            } else {
+                auto inserted = session->responses.insert(
+                                                {rpcInfo.rpc_number(), {}});
+                if (inserted.second) {
+                    // response not found, apply and save it
+                    Tree::ProtoBuf::readWriteTreeRPC(
+                        tree,
+                        command.tree(),
+                        *inserted.first->second.mutable_tree());
+                } else {
+                    // response exists, do not re-apply
+                }
+            }
+        }
     } else if (command.has_open_session()) {
-        openSession(entryId, command.open_session());
-        return;
+        uint64_t clientId = entryId;
+        session = &sessions.insert({clientId, {}}).first->second;
     } else {
         PANIC("unknown command at %lu: %s", entryId, data.c_str());
     }
 
-    Session& session = sessions[rpcInfo.client_id()];
-    if (session.firstOutstandingRPC < rpcInfo.first_outstanding_rpc())
-        session.firstOutstandingRPC = rpcInfo.first_outstanding_rpc();
-
-    // Discard unneeded responses in session
-    auto it = session.responses.begin();
-    while (it != session.responses.end()) {
-        if (it->first < session.firstOutstandingRPC)
-            it = session.responses.erase(it);
-        else
-            ++it;
+    if (command.has_nanoseconds_since_epoch()) {
+        if (session != NULL) {
+            session->lastModified = command.nanoseconds_since_epoch();
+            session = NULL; // pointer invalidated by expireSessions()
+        }
+        expireSessions(command.nanoseconds_since_epoch());
     }
-    // Add new response to session
-    session.responses[rpcInfo.rpc_number()] = commandResponse;
 }
-
 
 void
 StateMachine::applyThreadMain()
@@ -220,21 +239,42 @@ StateMachine::dumpSessionSnapshot(
     sessionsProto.SerializeWithCachedSizes(&stream);
 }
 
-bool
-StateMachine::ignore(const PC::ExactlyOnceRPCInfo& rpcInfo) const
+void
+StateMachine::expireResponses(Session& session, uint64_t firstOutstandingRPC)
 {
-    auto it = sessions.find(rpcInfo.client_id());
-    if (it == sessions.end())
-        return true; // no such session
-    const Session& session = it->second;
-    if (session.responses.find(rpcInfo.rpc_number()) !=
-        session.responses.end()) {
-        return true; // response exists
+    if (session.firstOutstandingRPC >= firstOutstandingRPC)
+        return;
+    session.firstOutstandingRPC = firstOutstandingRPC;
+    auto it = session.responses.begin();
+    while (it != session.responses.end()) {
+        if (it->first < session.firstOutstandingRPC)
+            it = session.responses.erase(it);
+        else
+            ++it;
     }
-    if (rpcInfo.rpc_number() < session.firstOutstandingRPC)
-        return true; // response already discarded
-    return false;
 }
+
+void
+StateMachine::expireSessions(uint64_t nanosecondsSinceEpoch)
+{
+    auto it = sessions.begin();
+    while (it != sessions.end()) {
+        Session& session = it->second;
+        uint64_t expireTime = session.lastModified + sessionTimeoutNanos;
+        if (expireTime < nanosecondsSinceEpoch) {
+            uint64_t diffNanos = nanosecondsSinceEpoch - session.lastModified;
+            NOTICE("Expiring client %lu's session after %lu.%09lu seconds "
+                   "due to inactivity",
+                   it->first,
+                   diffNanos / (1000 * 1000 * 1000UL),
+                   diffNanos % (1000 * 1000 * 1000UL));
+            it = sessions.erase(it);
+        } else {
+            ++it;
+        }
+    }
+}
+
 
 void
 StateMachine::loadSessionSnapshot(
@@ -267,15 +307,6 @@ StateMachine::loadSessionSnapshot(
             session.responses.insert({it2->rpc_number(), it2->response()});
         }
     }
-}
-
-void
-StateMachine::openSession(
-        uint64_t entryId,
-        const Protocol::Client::OpenSession::Request& request)
-{
-    uint64_t clientId = entryId;
-    sessions.insert({clientId, {}});
 }
 
 bool

Server/StateMachineTest.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013 Stanford University
+/* Copyright (c) 2013-2014 Stanford University
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -81,6 +81,100 @@ class ServerStateMachineTest : public ::testing::Test {
     std::unique_ptr<StateMachine> stateMachine;
     Storage::FilesystemUtil::File tmpdir;
 };
+
+TEST_F(ServerStateMachineTest, getResponse)
+{
+    Core::Debug::setLogPolicy({{"Server/StateMachine.cc", "ERROR"}});
+    stateMachine->sessions.insert({1, {}});
+    StateMachine::Session& session = stateMachine->sessions.at(1);
+    Protocol::Client::CommandResponse r1;
+    Protocol::Client::CommandResponse r2;
+    r1.mutable_tree()->set_status(Protocol::Client::Status::LOOKUP_ERROR);
+    session.responses.insert({1, r1});
+    Protocol::Client::ExactlyOnceRPCInfo rpcInfo;
+    rpcInfo.set_client_id(2);
+    rpcInfo.set_rpc_number(1);
+    EXPECT_FALSE(stateMachine->getResponse(rpcInfo, r2));
+    rpcInfo.set_client_id(1);
+    rpcInfo.set_rpc_number(2);
+    EXPECT_FALSE(stateMachine->getResponse(rpcInfo, r2));
+    Core::Debug::setLogPolicy({{"", "WARNING"}});
+    rpcInfo.set_client_id(1);
+    rpcInfo.set_rpc_number(1);
+    EXPECT_TRUE(stateMachine->getResponse(rpcInfo, r2));
+    EXPECT_EQ(r1, r2);
+}
+
+TEST_F(ServerStateMachineTest, apply_tree)
+{
+    stateMachine->sessionTimeoutNanos = 1;
+    Protocol::Client::Command command =
+        Core::ProtoBuf::fromString<Protocol::Client::Command>(
+            "nanoseconds_since_epoch: 2, "
+            "tree: { "
+            " exactly_once: { "
+            "  client_id: 39 "
+            "  first_outstanding_rpc: 2 "
+            "  rpc_number: 3 "
+            " } "
+            " make_directory { "
+            "  path: '/a' "
+            " } "
+            "}");
+    std::vector<std::string> children;
+
+    // session does not exist
+    stateMachine->sessions.insert({1, {}});
+    stateMachine->apply(6, Core::ProtoBuf::dumpString(command));
+    stateMachine->tree.listDirectory("/", children);
+    EXPECT_EQ((std::vector<std::string> {}), children);
+    ASSERT_EQ(0U, stateMachine->sessions.size());
+
+    // session exists and need to apply
+    stateMachine->sessions.insert({1, {}});
+    stateMachine->sessions.insert({39, {}});
+    stateMachine->apply(6, Core::ProtoBuf::dumpString(command));
+    stateMachine->tree.listDirectory("/", children);
+    EXPECT_EQ((std::vector<std::string> {"a/"}), children);
+    ASSERT_EQ(1U, stateMachine->sessions.size());
+    EXPECT_EQ(2U, stateMachine->sessions.at(39).lastModified);
+
+    // session exists and response exists
+    stateMachine->sessions.insert({1, {}});
+    stateMachine->tree.removeDirectory("/a");
+    stateMachine->apply(6, Core::ProtoBuf::dumpString(command));
+    stateMachine->tree.listDirectory("/", children);
+    EXPECT_EQ((std::vector<std::string> {}), children);
+    ASSERT_EQ(1U, stateMachine->sessions.size());
+    EXPECT_EQ(2U, stateMachine->sessions.at(39).lastModified);
+
+    // session exists but response discarded
+    stateMachine->sessions.insert({1, {}});
+    stateMachine->expireResponses(stateMachine->sessions.at(39), 4);
+    stateMachine->apply(6, Core::ProtoBuf::dumpString(command));
+    stateMachine->tree.listDirectory("/", children);
+    EXPECT_EQ((std::vector<std::string> {}), children);
+    ASSERT_EQ(1U, stateMachine->sessions.size());
+    EXPECT_EQ(2U, stateMachine->sessions.at(39).lastModified);
+}
+
+TEST_F(ServerStateMachineTest, apply_openSession)
+{
+    stateMachine->sessionTimeoutNanos = 1;
+    stateMachine->sessions.insert({1, {}});
+    Protocol::Client::Command command =
+        Core::ProtoBuf::fromString<Protocol::Client::Command>(
+            "nanoseconds_since_epoch: 2, "
+            "open_session: {}");
+    stateMachine->apply(6, Core::ProtoBuf::dumpString(command));
+    ASSERT_EQ((std::vector<uint64_t>{6U}),
+              Core::STLUtil::sorted(
+                  Core::STLUtil::getKeys(stateMachine->sessions)));
+    StateMachine::Session& session = stateMachine->sessions.at(6);
+    EXPECT_EQ(2U, session.lastModified);
+    EXPECT_EQ(0U, session.firstOutstandingRPC);
+    EXPECT_EQ(0U, session.responses.size());
+}
 
 // This tries to test the use of kill() to stop a snapshotting child and exit
 // quickly.
@@ -170,8 +264,41 @@ TEST_F(ServerStateMachineTest, dumpSessionSnapshot)
                     stateMachine->sessions.at(91).responses)));
 }
 
-// loadSessionSnapshot tested along with dumpSessionSnapshot above
+TEST_F(ServerStateMachineTest, expireResponses)
+{
+    stateMachine->sessions.insert({1, {}});
+    StateMachine::Session& session = stateMachine->sessions.at(1);
+    session.responses.insert({1, {}});
+    session.responses.insert({2, {}});
+    session.responses.insert({4, {}});
+    session.responses.insert({5, {}});
+    stateMachine->expireResponses(session, 4);
+    stateMachine->expireResponses(session, 3);
+    EXPECT_EQ(4U, session.firstOutstandingRPC);
+    EXPECT_EQ((std::vector<uint64_t>{4U, 5U}),
+              Core::STLUtil::sorted(
+                  Core::STLUtil::getKeys(session.responses)));
+}
 
+TEST_F(ServerStateMachineTest, expireSessions)
+{
+    stateMachine->sessionTimeoutNanos = 1;
+    stateMachine->sessions.insert({1, {}});
+    stateMachine->sessions.at(1).lastModified = 100;
+    stateMachine->sessions.insert({2, {}});
+    stateMachine->sessions.at(2).lastModified = 400;
+    stateMachine->sessions.insert({3, {}});
+    stateMachine->sessions.at(3).lastModified = 200;
+    stateMachine->sessions.insert({4, {}});
+    stateMachine->sessions.at(4).lastModified = 201;
+    stateMachine->sessions.insert({5, {}});
+    stateMachine->expireSessions(202);
+    EXPECT_EQ((std::vector<uint64_t>{2U, 4U}),
+              Core::STLUtil::sorted(
+                  Core::STLUtil::getKeys(stateMachine->sessions)));
+}
+
+// loadSessionSnapshot tested along with dumpSessionSnapshot above
 
 TEST_F(ServerStateMachineTest, takeSnapshot)
 {
@@ -193,7 +320,8 @@ TEST_F(ServerStateMachineTest, takeSnapshot)
     stateMachine->tree.listDirectory("/", children);
     EXPECT_EQ((std::vector<std::string>{"foo/"}), children);
     EXPECT_EQ((std::vector<std::uint64_t>{4}),
-              Core::STLUtil::getKeys(stateMachine->sessions));
+              Core::STLUtil::sorted(
+                  Core::STLUtil::getKeys(stateMachine->sessions)));
 }
 
 } // namespace LogCabin::Server::<anonymous>

Client/Client.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012 Stanford University
+/* Copyright (c) 2011-2014 Stanford University
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -416,6 +416,14 @@ class Tree {
 
 /**
  * A handle to the LogCabin cluster.
+ *
+ * If the client requests changes to the cluster's replicated state machine
+ * (for example, by writing a value), the client library will first open a
+ * session with the cluster. It will thereafter periodically send keep-alive
+ * requests to the cluster during periods of inactivity to maintain this
+ * session. If communication to the LogCabin cluster is lost for an extended
+ * period of time, the client's session will expire, and this library will
+ * force the client to crash.
  */
 class Cluster {
   public:

Protocol/Client.proto
@@ -291,6 +291,14 @@ message ReadWriteTree {
  * This is what the state machine takes in from the replicated log.
  */
 message Command {
+    /**
+     * The leader's time when it received the command, measured in nanoseconds
+     * since the Unix epoch. Useful for cleaning up client sessions. Note that
+     * this may occasionally go backwards due to leader changes or time
+     * synchronization.
+     */
+    optional uint64 nanoseconds_since_epoch = 1;
+
     // The following are mutually exclusive.
     optional OpenSession.Request open_session = 4;
     optional ReadWriteTree.Request tree = 5;

Server/ClientService.cc
@@ -24,6 +24,21 @@
 #include "Server/Globals.h"
 #include "Server/StateMachine.h"
 
+namespace {
+
+/**
+ * Return the time since the Unix epoch in nanoseconds.
+ */
+uint64_t timeNanos()
+{
+    struct timespec now;
+    int r = clock_gettime(CLOCK_REALTIME, &now);
+    assert(r == 0);
+    return uint64_t(now.tv_sec) * 1000 * 1000 * 1000 + now.tv_nsec;
+}
+
+}
+
 namespace LogCabin {
 namespace Server {
 
@@ -108,6 +123,8 @@ std::pair<Result, uint64_t>
 ClientService::submit(RPC::ServerRPC& rpc,
                       const google::protobuf::Message& command)
 {
+    // TODO(ongaro): Switch from string to binary format. This is probably
+    // really slow to serialize.
     std::string cmdStr = Core::ProtoBuf::dumpString(command);
     std::pair<Result, uint64_t> result = globals.raft->replicate(cmdStr);
     if (result.first == Result::RETRY || result.first == Result::NOT_LEADER) {
@@ -154,6 +171,7 @@ ClientService::openSession(RPC::ServerRPC rpc)
 {
     PRELUDE(OpenSession);
     Command command;
+    command.set_nanoseconds_since_epoch(timeNanos());
     *command.mutable_open_session() = request;
     std::pair<Result, uint64_t> result = submit(rpc, command);
     if (result.first != Result::SUCCESS)
@@ -230,6 +248,7 @@ ClientService::readWriteTreeRPC(RPC::ServerRPC rpc)
 {
     PRELUDE(ReadWriteTree);
     Command command;
+    command.set_nanoseconds_since_epoch(timeNanos());
     *command.mutable_tree() = request;
     std::pair<Result, uint64_t> result = submit(rpc, command);
     if (result.first != Result::SUCCESS)

Server/StateMachine.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013 Stanford University
+/* Copyright (c) 2012-2014 Stanford University
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -64,6 +64,9 @@ class StateMachine {
     void wait(uint64_t entryId) const;
 
   private:
+    // forward declaration
+    struct Session;
+
     /**
      * Invoked once per committed entry from the Raft log.
      */
@@ -81,22 +84,27 @@ class StateMachine {
                 google::protobuf::io::CodedOutputStream& stream) const;
 
     /**
-     * Return true if the state machine should ignore the command (because it
-     * is a duplicate of a previous command).
+     * Update the session and clean up unnecessary responses.
+     * \param session
+     *      Affected session.
+     * \param firstOutstandingRPC
+     *      New value for the first outstanding RPC for a session.
      */
-    bool ignore(const Protocol::Client::ExactlyOnceRPCInfo& rpcInfo) const;
+    void expireResponses(Session& session, uint64_t firstOutstandingRPC);
+
+    /**
+     * Remove old sessions.
+     * \param nanosecondsSinceEpoch
+     *      Sessions that have not been used since this (leader) time are
+     *      removed.
+     */
+    void expireSessions(uint64_t nanosecondsSinceEpoch);
 
     /**
      * Read the #sessions table from a snapshot file.
      */
     void loadSessionSnapshot(
                 google::protobuf::io::CodedInputStream& stream);
-
-    /**
-     * Allocate a new Session upon a client request to do so.
-     */
-    void openSession(uint64_t entryId,
-                     const Protocol::Client::OpenSession::Request& request);
 
     /**
      * Return true if it is time to create a new snapshot.
@@ -116,8 +124,6 @@ class StateMachine {
     void takeSnapshot(uint64_t lastIncludedIndex,
                       std::unique_lock<std::mutex>& lockGuard);
 
-
-
     std::shared_ptr<Consensus> consensus;
 
     /**
@@ -130,6 +136,12 @@ class StateMachine {
      * snapshot.
      */
     uint64_t snapshotRatio;
+
+    /**
+     * The time interval after which to remove an inactive client session, in
+     * nanoseconds of leader time.
+     */
+    uint64_t sessionTimeoutNanos;
 
     /**
      * Protects against concurrent access for all members of this class (except
@@ -177,13 +189,18 @@ class StateMachine {
      */
     struct Session {
         Session()
-            : firstOutstandingRPC(0)
+            : lastModified(0)
+            , firstOutstandingRPC(0)
             , responses()
         {
         }
         /**
+         * When the session was last active, measured in leader time
+         * nanoseconds since the Unix epoch.
+         */
+        uint64_t lastModified;
+        /**
          * Largest firstOutstandingRPC number processed from this client.
-         * (RPCs that are ignored do not count for this purpose.)
          */
         uint64_t firstOutstandingRPC;
         /**
@@ -197,7 +214,6 @@ class StateMachine {
 
     /**
      * Client ID to Session map.
-     * TODO(ongaro): Will need to clean up stale sessions somehow.
      */
     std::unordered_map<uint64_t, Session> sessions;
 

commit 445f3839f45a1615f25f332211ed7449b4f80666

Fix critical bug in session expiration after loading snapshot
This problem was introduced two days ago in commit 10b42c6ff. Session
modify times weren't being saved in snapshots, so after loading a
snapshot, a server would expire sessions too aggressively. This would
potentially cause state machines to diverge (resulting in consistency
problems).

The fix is straightforward: save the session modification time in the
snapshot along with the rest of the session fields.

Server/Sessions.proto
@@ -34,6 +34,7 @@ message Sessions {
  */
 message Session {
     required uint64 client_id = 1;
+    required uint64 last_modified = 4;
     required uint64 first_outstanding_rpc = 2;
     repeated Response rpc_response = 3;
 }

Server/StateMachine.cc
@@ -223,6 +223,7 @@ StateMachine::dumpSessionSnapshot(
     for (auto it = sessions.begin(); it != sessions.end(); ++it) {
         SessionsProto::Session& session = *sessionsProto.add_session();
         session.set_client_id(it->first);
+        session.set_last_modified(it->second.lastModified);
         session.set_first_outstanding_rpc(it->second.firstOutstandingRPC);
         for (auto it2 = it->second.responses.begin();
              it2 != it->second.responses.end();
@@ -300,6 +301,7 @@ StateMachine::loadSessionSnapshot(
          ++it) {
         Session& session = sessions.insert({it->client_id(), {}})
                                                         .first->second;
+        session.lastModified = it->last_modified();
         session.firstOutstandingRPC = it->first_outstanding_rpc();
         for (auto it2 = it->rpc_response().begin();
              it2 != it->rpc_response().end();

Server/StateMachineTest.cc
@@ -212,6 +212,7 @@ TEST_F(ServerStateMachineTest, dumpSessionSnapshot)
     r1.mutable_tree()->set_status(Protocol::Client::Status::TYPE_ERROR);
 
     StateMachine::Session s1;
+    s1.lastModified = 6;
     s1.firstOutstandingRPC = 5;
     s1.responses.insert({5, r1});
     s1.responses.insert({7, r2});
@@ -243,6 +244,7 @@ TEST_F(ServerStateMachineTest, dumpSessionSnapshot)
     EXPECT_EQ((std::vector<std::uint64_t>{4, 80, 91}),
               Core::STLUtil::sorted(
                 Core::STLUtil::getKeys(stateMachine->sessions)));
+    EXPECT_EQ(6U, stateMachine->sessions.at(4).lastModified);
     EXPECT_EQ(5U, stateMachine->sessions.at(4).firstOutstandingRPC);
     EXPECT_EQ(9U, stateMachine->sessions.at(80).firstOutstandingRPC);
     EXPECT_EQ(6U, stateMachine->sessions.at(91).firstOutstandingRPC);