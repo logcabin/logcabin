bd40cf9e26ef323db55c60c62ee2b03436b44d82
Diego Ongaro <dongaro@scalecomputing.com>
2015-01-05 14:42:48 -0800

    Fix build on g++-4.9 (Core/Time.h)

--- Core/Time.h
+++ Core/Time.h
@@ -23,48 +23,6 @@
 #ifndef LOGCABIN_CORE_TIME_H
 #define LOGCABIN_CORE_TIME_H
 
-namespace std {
-
-/**
- * Prints std::milliseconds values in a way that is useful for unit tests.
- */
-inline std::ostream&
-operator<<(std::ostream& os,
-           const std::chrono::milliseconds& duration) {
-    return os << duration.count() << " ms";
-}
-
-/**
- * Prints std::nanoseconds values in a way that is useful for unit tests.
- */
-inline std::ostream&
-operator<<(std::ostream& os,
-           const std::chrono::nanoseconds& duration) {
-    return os << duration.count() << " ns";
-}
-
-
-/**
- * Prints std::time_point values in a way that is useful for unit tests.
- */
-template<typename Clock, typename Duration>
-std::ostream&
-operator<<(std::ostream& os,
-           const std::chrono::time_point<Clock, Duration>& timePoint) {
-    typedef std::chrono::time_point<Clock, Duration> TimePoint;
-    using LogCabin::Core::StringUtil::format;
-
-    if (timePoint == TimePoint::min())
-        return os << "TimePoint::min()";
-    if (timePoint == TimePoint::max())
-        return os << "TimePoint::max()";
-
-    struct timespec ts = makeTimeSpec(timePoint);
-    return os << format("%ld.%09ld", ts.tv_sec, ts.tv_nsec);
-}
-
-}
-
 namespace LogCabin {
 namespace Core {
 namespace Time {
@@ -234,5 +192,47 @@ uint64_t getTimeNanos();
 } // namespace LogCabin::Core::Time
 } // namespace LogCabin::Core
 } // namespace LogCabin
+
+namespace std {
+
+/**
+ * Prints std::milliseconds values in a way that is useful for unit tests.
+ */
+inline std::ostream&
+operator<<(std::ostream& os,
+           const std::chrono::milliseconds& duration) {
+    return os << duration.count() << " ms";
+}
+
+/**
+ * Prints std::nanoseconds values in a way that is useful for unit tests.
+ */
+inline std::ostream&
+operator<<(std::ostream& os,
+           const std::chrono::nanoseconds& duration) {
+    return os << duration.count() << " ns";
+}
+
+
+/**
+ * Prints std::time_point values in a way that is useful for unit tests.
+ */
+template<typename Clock, typename Duration>
+std::ostream&
+operator<<(std::ostream& os,
+           const std::chrono::time_point<Clock, Duration>& timePoint) {
+    typedef std::chrono::time_point<Clock, Duration> TimePoint;
+    using LogCabin::Core::StringUtil::format;
+
+    if (timePoint == TimePoint::min())
+        return os << "TimePoint::min()";
+    if (timePoint == TimePoint::max())
+        return os << "TimePoint::max()";
+
+    struct timespec ts = LogCabin::Core::Time::makeTimeSpec(timePoint);
+    return os << format("%ld.%09ld", ts.tv_sec, ts.tv_nsec);
+}
+
+} // namespace std
 
 #endif /* LOGCABIN_CORE_TIME_H */
