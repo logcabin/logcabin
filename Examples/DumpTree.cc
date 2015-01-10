5a80e26a95171afff0d8112a1e0deb3771b428ab
Diego Ongaro <dongaro@scalecomputing.com>
2015-01-09 16:13:44 -0800

    Add DumpTree client to print out the contents of the LogCabin tree

---
+++ Examples/DumpTree.cc
@@ -0,0 +1,125 @@
+/* Copyright (c) 2012 Stanford University
+ * Copyright (c) 2015 Diego Ongaro
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <cassert>
+#include <getopt.h>
+#include <iostream>
+
+#include <LogCabin/Client.h>
+
+namespace {
+
+using LogCabin::Client::Cluster;
+using LogCabin::Client::Tree;
+
+/**
+ * Parses argv for the main function.
+ */
+class OptionParser {
+  public:
+    OptionParser(int& argc, char**& argv)
+        : argc(argc)
+        , argv(argv)
+        , cluster("logcabin:61023")
+        , timeout(0)
+    {
+        while (true) {
+            static struct option longOptions[] = {
+               {"cluster",  required_argument, NULL, 'c'},
+               {"timeout",  required_argument, NULL, 't'},
+               {"help",  no_argument, NULL, 'h'},
+               {0, 0, 0, 0}
+            };
+            int c = getopt_long(argc, argv, "c:t:h", longOptions, NULL);
+
+            // Detect the end of the options.
+            if (c == -1)
+                break;
+
+            switch (c) {
+                case 'c':
+                    cluster = optarg;
+                    break;
+                case 't':
+                {
+                    char* end;
+                    timeout = strtoull(optarg, &end, 10);
+                    break;
+                }
+                case 'h':
+                    usage();
+                    exit(0);
+                case '?':
+                default:
+                    // getopt_long already printed an error message.
+                    usage();
+                    exit(1);
+            }
+        }
+    }
+
+    void usage() {
+        std::cout << "Usage: " << argv[0] << " [options] <servers>"
+                  << std::endl;
+        std::cout << "Options: " << std::endl;
+        std::cout << "  -c, --cluster <address> "
+                  << "The network address of the LogCabin cluster "
+                  << "(default: logcabin:61023)" << std::endl;
+        std::cout << "  -t, --timeout           "
+                  << "Set timeout in seconds (default: 0, wait forever)"
+                  << std::endl;
+        std::cout << "  -h, --help              "
+                  << "Print this usage information" << std::endl;
+    }
+
+    int& argc;
+    char**& argv;
+    std::string cluster;
+    uint64_t timeout;
+};
+
+/**
+ * Depth-first search tree traversal, dumping out contents of all files
+ */
+void
+dumpTree(const Tree& tree, std::string path)
+{
+    std::cout << path << std::endl;
+    std::vector<std::string> children = tree.listDirectoryEx(path);
+    for (auto it = children.begin(); it != children.end(); ++it) {
+        std::string child = path + *it;
+        if (*child.rbegin() == '/') { // directory
+            dumpTree(tree, child);
+        } else { // file
+            std::cout << child << ": " << std::endl;
+            std::cout << "    " << tree.readEx(child) << std::endl;
+        }
+    }
+}
+
+} // anonymous namespace
+
+int
+main(int argc, char** argv)
+{
+    OptionParser options(argc, argv);
+    Cluster cluster(options.cluster);
+    Tree tree = cluster.getTree();
+    tree.setTimeout(options.timeout * 1000000000);
+    dumpTree(tree, "/");
+    return 0;
+}
+
