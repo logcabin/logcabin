2cebdb0c17c98deda040625bfccf7f54d65594e1
Diego Ongaro <dongaro@scalecomputing.com>
2015-05-13 16:42:01 -0700

    Update Core/Time.h ifdefs for Clang

--- Core/Time.h
+++ Core/Time.h
@@ -167,7 +167,12 @@ MockableClock<BaseClock>::mockValue;
 // libstdc++ 4.7 renamed monotonic_clock to steady_clock to conform with C++11.
 // libstdc++ 4.8 seems to be the first version where std::chrono::steady_clock
 // is usable with nanosecond granularity.
-#if __GNUC__ == 4 && __GNUC_MINOR__ < 8
+// Clang with libstdc++ gives us no way to check the version,
+// so we just use CSteadyClock in that case.
+// Clang with libc++ is known to work at _LIBCPP_VERSION 1101, and below hasn't
+// been tried (use CSteadyClock).
+#if (__clang__ && (!defined(_LIBCPP_VERSION) || _LIBCPP_VERSION < 1101)) || \
+    (!__clang__ && __GNUC__ == 4 && __GNUC_MINOR__ < 8)
 typedef MockableClock<CSteadyClock> SteadyClock;
 #else
 typedef MockableClock<std::chrono::steady_clock> SteadyClock;
@@ -178,7 +183,12 @@ typedef MockableClock<std::chrono::steady_clock> SteadyClock;
  */
 // libstdc++ 4.8 seems to be the first version where std::chrono::system_clock
 // has nanosecond granularity.
-#if __GNUC__ == 4 && __GNUC_MINOR__ < 8
+// Clang with libstdc++ gives us no way to check the version,
+// so we just use CSystemClock in that case.
+// Clang with libc++ is known to work at _LIBCPP_VERSION 1101, and below hasn't
+// been tried (use CSystemClock).
+#if (__clang__ && (!defined(_LIBCPP_VERSION) || _LIBCPP_VERSION < 1101)) || \
+    (!__clang__ && __GNUC__ == 4 && __GNUC_MINOR__ < 8)
 typedef MockableClock<CSystemClock> SystemClock;
 #else
 typedef MockableClock<std::chrono::system_clock> SystemClock;
